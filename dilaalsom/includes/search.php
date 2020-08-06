f6d3dd01561f36a9c046e19c1a0d7c572947fb74
Abdulmajeed A. Raji <45627583+atoyebii@users.noreply.github.com>
2020-08-06 17:40:26 +0200

    Update search.php

--- dilaalsom/includes/search.php
+++ dilaalsom/includes/search.php
@@ -1,59 +1,46 @@
 <?php
-		require_once 'dbconnect.php';
-			
-			if( isset($_GET["q"]) ){
-			$q	= 	$_GET["q"];
-		}else{
-			$q	= 	"";	
-		}
+require_once 'dbconnect.php';
 
-		if( isset($_GET["category"]) ){
-			$cat	= 	$_GET["category"];
-		}else{
-			$cat	= 	"";	
-		}
-		
-	$query = "SELECT a.*, c.county FROM adverts a
-			INNER JOIN counties c ON (a.location = c.id_county)
-			WHERE a.title LIKE '%".$q."%' 
-			AND a.id_category LIKE '%".$cat."%'
-			ORDER BY a.id_advert DESC;";
-	
-	
+if (isset($_GET["q"])) {
+	$q	= 	$_GET["q"];
+} else {
+	$q	= 	"";
+}
 
-		if( ! $result = mysqli_query($link, $query) ){
-			echo mysqli_error($link);
-			exit();
-			}
-		
-		$numrows =mysqli_num_rows($result);
-		if( mysqli_num_rows($result) < 1)
-			{
-				$adverts = "not results";
-			}else{
-						
-			
-		$adverts = "";
-		while($row = mysqli_fetch_array($result)){
-		$adverts.= ' 	<div class="col s12 m4">
+if (isset($_GET["category"])) {
+	$cat	= 	$_GET["category"];
+} else {
+	$cat	= 	"";
+}
+
+$query = "SELECT * FROM advertisements";
+
+
+if (!$result = mysqli_query($link, $query)) {
+	echo mysqli_error($link);
+	exit();
+}
+
+$numrows = mysqli_num_rows($result);
+if (mysqli_num_rows($result) < 1) {
+	$adverts = "not results";
+} else {
+	$adverts = "";
+	while ($row = mysqli_fetch_array($result)) {
+		$adverts .= ' 	<div class="col s12 m4">
 						<div class="card small">
-              			<div class="card-image">
-                			<img src="'.$row["main_picture"].'">
-                			<span class="card-title">'.$row["title"].'</span>
+							<span class=""> ' . $row["title"] . '</span>
+						  <div class="card-image">
+						  <img src="/dilaalsom/img/products/' . $row["images"] . '">
+
               			</div>
-              			<div class="card-content">
-               				<div class="price">€ '.$row["price"].'</div>
-							<div class="county"> '.$row["county"].'</div>	
+						  <div class="card-content">
+               				<div class="price">€ ' . $row["price"] . '</div>
+							<div class="state">State: ' . $row["state"] . '</div>	
+							<div class="district">District: ' . $row["district"] . '</div>	
               			</div>
               			<div class="card-action">
-                		<a href="viewad.php?idadvert='.$row["id_advert"].'" class="viewad">View Ad</a>
+                		<a href="viewad.php?idadvert=' . $row["id"] . '" class="viewad">View Ad</a>
               		</div></div></div>';
-			}}
-			?>
-
-	
-		
-		
-		
-	
-
+	}
+}
