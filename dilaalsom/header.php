c4181aa0c41e86edc154955c9d39a782ffb2e899
Abdulmajeed A. Raji <45627583+atoyebii@users.noreply.github.com>
2020-08-07 23:07:24 +0200

    Update header.php

--- dilaalsom/header.php
+++ dilaalsom/header.php
@@ -8,6 +8,171 @@ include 'includes/function.php';
 
 <head>
   <!--Import Google Icon Font-->
+
+  <link rel="shortcut icon" type="image/x-icon" href="https://localhost/dilaalsom/logo1.jpg" />
+  <link rel="icon" href="https://localhost/dilaalsom/logo1.jpg" sizes="16x16" type="image/png" />
+  <link rel="icon" href="https://localhost/dilaalsom/logo1.jpg" sizes="32x32" type="image/png" />
+  <!--<link rel="mask-icon" href="https://salamamarket.com/oc-content/themes/gamma/images/favicons/safari-pinned-tab.svg" color="#8bc72a">-->
+  <!--<meta name="theme-color" content="#8bc72a">-->
+  <style>
+    .mbCl,
+    footer .cl .lnk:hover,
+    header .right a:hover,
+    header .right a.publish:hover,
+    body a,
+    body a:hover {
+      color: #4182c3;
+    }
+
+    .mbCl2 {
+      color: #49b975;
+    }
+
+    .mbCl3,
+    header .right a.actv,
+    header .right a.actv svg {
+      color: #ef404f;
+    }
+
+    .mbBg,
+    .pace .pace-progress,
+    body #show-loan i,
+    .im-body #uniform-undefined.frm-category,
+    .frm-answer .frm-area .frm-buttons button,
+    #search-sort .user-type a.active,
+    #search-sort .list-grid a.active,
+    .paginate ul li span,
+    #listing .data .connect-after a:hover,
+    .paginate ul li a:hover,
+    .blg-btn.blg-btn-primary,
+    .bpr-prof .bpr-btn,
+    .post-edit .price-wrap .selection a.active {
+      background: #4182c3 !important;
+      color: #fff !important;
+    }
+
+    .mbBg2,
+    .im-button-green {
+      background: #49b975 !important;
+      color: #fff !important;
+    }
+
+    .mbBg3,
+    .mobile-box a i,
+    #photos .qq-upload-button,
+    .tabbernav li.tabberactive a,
+    .frm-title-right a.frm-new-topic,
+    .im-user-account-count {
+      background: #ef404f !important;
+      color: #fff !important;
+    }
+
+    .mbBgAf:after {
+      background: #4182c3 !important;
+    }
+
+    .mbBgActive.active {
+      background: #4182c3 !important;
+    }
+
+    .mbBg2Active.active {
+      background: #49b975 !important;
+    }
+
+    .mbBg3Active.active {
+      background: #ef404f !important;
+    }
+
+      {
+      background-color: #4182c3 !important;
+    }
+
+    .mbBr,
+    header .right a.publish:hover {
+      border-color: #4182c3 !important;
+    }
+
+    .mbBr2 {
+      border-color: #49b975 !important;
+    }
+
+    .mbBr3,
+    .user-top-menu>.umenu li.active a {
+      border-color: #ef404f !important;
+    }
+
+    #atr-search .atr-input-box input[type="checkbox"]:checked+label:before,
+    #atr-search .atr-input-box input[type="radio"]:checked+label:before,
+    #atr-form .atr-input-box input[type="checkbox"]:checked+label:before,
+    #atr-form .atr-input-box input[type="radio"]:checked+label:before,
+    .bpr-box-check input[type="checkbox"]:checked+label:before,
+    #gdpr-check.styled .input-box-check input[type="checkbox"]:checked+label:before,
+    .pol-input-box input[type="checkbox"]:checked+label:before,
+    .pol-values:not(.pol-nm-star) .pol-input-box input[type="radio"]:checked+label:before {
+      border-color: #4182c3 !important;
+      background-color: #4182c3 !important;
+    }
+
+      {
+      border-bottom-color: #4182c3 !important;
+    }
+
+    .mbBr2Top {
+      border-top-color: #49b975 !important;
+    }
+
+    .mbBr3Top,
+    body #fi_user_new_list {
+      border-top-color: #ef404f !important;
+    }
+  </style>
+
+  <script>
+    var mbCl = '.mbCl,footer .cl .lnk:hover,header .right a:hover, header .right a.publish:hover, body a, body a:hover';
+    var mbCl2 = '.mbCl2';
+    var mbCl3 = '.mbCl3,header .right a.actv,header .right a.actv svg';
+    var mbBg = '.mbBg,.pace .pace-progress,body #show-loan i, .im-body #uniform-undefined.frm-category,.frm-answer .frm-area .frm-buttons button,#search-sort .user-type a.active,#search-sort .list-grid a.active,.paginate ul li span,#listing .data .connect-after a:hover,.paginate ul li a:hover,.blg-btn.blg-btn-primary,.bpr-prof .bpr-btn, .post-edit .price-wrap .selection a.active';
+    var mbBg2 = '.mbBg2, .im-button-green';
+    var mbBg3 = '.mbBg3,.mobile-box a i,#photos .qq-upload-button, .tabbernav li.tabberactive a,.frm-title-right a.frm-new-topic,.im-user-account-count';
+    var mbBgAf = '.mbBgAf:after';
+    var mbBgAc = '.mbBgActive.active';
+    var mbBg2Ac = '.mbBg2Active.active';
+    var mbBg3Ac = '.mbBg3Active.active';
+    var mbBr = '.mbBr,header .right a.publish:hover';
+    var mbBr2 = '';
+    var mbBr3 = '';
+    var mbBrBg = '#atr-search .atr-input-box input[type="checkbox"]:checked + label:before, #atr-search .atr-input-box input[type="radio"]:checked + label:before,#atr-form .atr-input-box input[type="checkbox"]:checked + label:before, #atr-form .atr-input-box input[type="radio"]:checked + label:before,.bpr-box-check input[type="checkbox"]:checked + label:before, #gdpr-check.styled .input-box-check input[type="checkbox"]:checked + label:before, .pol-input-box input[type="checkbox"]:checked + label:before, .pol-values:not(.pol-nm-star) .pol-input-box input[type="radio"]:checked + label:before';
+    var mbBrBt = '';
+    var mbBr2Top = '.mbBr2Top';
+    var mbBr3Top = '.mbBr3Top, body #fi_user_new_list';
+  </script>
+
+
+
+
+  <meta name="generator" content="Osclass 3.8.0" />
+  <meta property="og:site_name" content="SALAMA MARKET" />
+  <meta property="og:locale" content="en_US" />
+  <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700|Encode+Sans+Condensed:600,700&display=swap" rel="stylesheet" type="text/css" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      var options = new Object();
+      options.cookieAcceptButton = true;
+      options.cookieDeclineButton = true;
+      options.cookiePolicyLink = 'https://salamamarket.com/';
+      options.cookieWhatAreTheyLink = 'http://www.allaboutcookies.org/';
+      options.cookieAnalyticsMessage = 'We use cookies, just to track visits to our website, we store no personal details.';
+      options.cookieMessage = 'We use cookies on this website, you can <a href="{{cookiePolicyLink}}" title="read about our cookies">read about them here</a>. To use the website as intended please...';
+      options.cookieAnalytics = false;
+      $.cookieCuttr(options);
+    });
+  </script>
+  <script data-ad-client="ca-pub-9687196717745782" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+
+
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Import materialize.css-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
@@ -55,9 +220,8 @@ include 'includes/function.php';
   </script>
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>
 
-    DilaalSom</title>
+  <title> Dilaal Somali Market </title>
   <meta name="title" content=" Dilaal Somali Advertsiment Market" />
   <meta name="description" content="Largest Advertisment platorm in Somali" />
   <meta http-equiv="Pragma" content="no-cache">
@@ -68,12 +232,15 @@ include 'includes/function.php';
 </head>
 
 <body id="home" class="scrollspy">
+
   <!-- Navbar -->
   <div class="navbar-fixed">
     <nav class="teal">
       <div class="container">
         <div class="nav-wrapper">
-          <a href="index.php" class="brand-logo">Dilaalsom</a>
+          <a href="index.php" class="brand-logo">
+            <img border="0" alt="Dilaalsom" width='60' height='62' src="https://localhost/dilaalsom/logo1.jpg" /></a>
+          </a>
           <a href="#" data-target="mobile-nav" class="sidenav-trigger">
             <i class="material-icons">menu</i>
           </a>
